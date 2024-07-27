04a66bc55dab21653d64e6b6606daa7fefc3efe7
bd8z <101759710+bd8z@users.noreply.github.com>
2024-07-27 14:36:06 +0900

    chore: Update README with license information and fix typo in main.py

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # README
-upload sensor data to Iot Core via MQTT
+
+## License Information
+This project includes code from **micropython-lib**, which is licensed under the MIT License. The original project can be found at [url](https://github.com/micropython/micropython-lib).
+
+- umqtt/simple.py
+
+## What is this/
+upload your sensor data to AWS Iot Core via MQTT
 
 more information:
 https://qiita.com/bd8z/items/e12e6885b627fc444eeb
+
--- src/main.py
+++ src/main.py
@@ -1,37 +1,45 @@
 import json
+import ssl
 import time
 
 import machine
 import network
+import ntptime
 import ubinascii
 from umqtt.simple import MQTTClient
 
-iot_core_endpoint = '{your-end-point}.amazonaws.com'
-mqtt_topic ='dt/homeCtrl/tokyo/'
+iot_core_endpoint = "{your-end-point}.amazonaws.com"
+mqtt_topic = "dt/homeCtrl/tokyo/"
 wifi_access_point = "{yourSSID}"
 wifi_password = "{yourSSID-wifi_password}"
 
+
 def getClientID():
-    mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
+    mac = ubinascii.hexlify(network.WLAN().config("mac"), ":").decode()
     return "picoW_" + mac
 
-def get_ssl_params():
-    keyfile = '/certs/private.der'
-    with open(keyfile, 'rb') as f:
-        key = f.read()
+
+def get_ssl_context():
+    keyfile = "/certs/private.der"
     certfile = "/certs/certificate.der"
-    with open(certfile, 'rb') as f:
-        cert = f.read()    
-    ssl_params = {'key': key,'cert': cert, 'server_side': False}
-    return ssl_params
+    cafile = "/certs/routeca.der"
+
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.load_verify_locations(cafile=cafile)
+    context.load_cert_chain(certfile, keyfile)
+    return context
+
 
 def conncet_wifi():
-    wlan = network.WLAN( network.STA_IF )
-    wlan.active( True )
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(False)
     time.sleep_ms(1000)
+    wlan.active(True)
+    time.sleep_ms(3000)
     retry = 10
-    while retry >0:
-        wlan.connect( wifi_access_point, wifi_password )
+    while retry > 0:
+        wlan.connect(wifi_access_point, wifi_password)
         timeout = 10
         while not wlan.isconnected() and timeout > 0:
             time.sleep_ms(1000)
@@ -44,17 +52,20 @@ def conncet_wifi():
             break
         retry -= 1
 
+
 def read_sensor():
     sensor = machine.ADC(2)
     factor = 3.3 / 65535
     sensor_volt = sensor.read_u16() * factor
     return sensor_volt
 
+
 def getBinaryMessage(value1):
     message = {}
     message["illuminance"] = value1
     data = json.dumps(message).encode()
     return data
+
 
 # initialize
 led = machine.Pin("LED", machine.Pin.OUT)
@@ -63,12 +74,15 @@ led.off()
 # wifi connect
 conncet_wifi()
 
-ssl_params=get_ssl_params()
+ntptime.settime()
+ssl_context = get_ssl_context()
 led.on()
 
 # mqtt connect
 mqtt_client_id = getClientID()
-mqtt = MQTTClient( mqtt_client_id, iot_core_endpoint, port = 8883, keepalive = 10000, ssl = True, ssl_params = ssl_params )
+mqtt = MQTTClient(
+    mqtt_client_id, iot_core_endpoint, port=8883, keepalive=10000, ssl=ssl_context
+)
 mqtt.connect()
 
 # send data
@@ -78,8 +92,9 @@ while True:
         for i in range(100):
             Msg = getBinaryMessage(read_sensor())
             print(Msg)
-            mqtt.publish( topic = mqtt_topic + mqtt_client_id, msg = Msg , qos = 0 )
-            time.sleep_ms(60*1000)
+            print(mqtt_topic + mqtt_client_id)
+            mqtt.publish(topic=mqtt_topic + mqtt_client_id, msg=Msg, qos=0)
+            time.sleep_ms(60 * 1000)
     except:
         led.off()
         conncet_wifi()
@@ -87,5 +102,5 @@ while True:
             mqtt.disconnect()
             time.sleep_ms(1000)
         except:
-            pass     
+            pass
         mqtt.connect()
